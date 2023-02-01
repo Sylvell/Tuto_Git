144934ba9b121443619fb3d685b892e9bf9d6a12
adrien alteirac <adrienalteirac@MacBook-Air-de-adrien.local>
2023-02-01 09:25:36 +0100

    ajout commentairecontrôleur

--- src/tuto_git/Contrôleur.java
+++ src/tuto_git/Contrôleur.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Contrôleur implements ActionListener {
-
+	// coucou commentaire non désiré
 	Modèle refModl;
 	
 	public Contrôleur(Modèle m) {
